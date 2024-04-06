fb8b98be56ced7ff4d55106f1122c99dad6eb5fd
Alexandru Mihai <alexandrumihai.03@icloud.com>
2024-04-07 00:10:23 +0300

    adaugare bootstrap, functii App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
 import { UserList } from './components/UserList';
-
-export default function App() {
+import PostList from './components/PostList';
+function App() {
   const [apiUsers, setApiUsers] = useState([]);
+  const [apiPosts, setApiPosts] = useState([]);
+  const [displayUsers, setDisplayUsers] = useState(true);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -11,11 +13,32 @@ export default function App() {
       .then((data) => {
         setApiUsers(data);
       });
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        setApiPosts(data);
+      });
   }, []);
 
+  const handleDisplayUsers = () => {
+    setDisplayUsers(true);
+  };
+
+  const handleDisplayPosts = () => {
+    setDisplayUsers(false);
+  };
+
   return (
-    <div className="App">
-      <UserList users={apiUsers} />
+    <div className="App container mt-5">
+    <div className="d-flex justify-content-center mb-3">
+      <button className="btn btn-custom m-2" onClick={handleDisplayUsers}>Afiseaza Utilizatorii</button>
+      <button className="btn btn-custom m-2" onClick={handleDisplayPosts}>Afiseaza Postarile</button>
     </div>
+    {displayUsers ? <UserList users={apiUsers} /> : <PostList posts={apiPosts} />}
+  </div>
   );
 }
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
