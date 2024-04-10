dc11875520ff3d8e10787132ed9c414c6d80e3e2
Alexandru Mihai <alexandrumihai.03@icloud.com>
2024-04-10 16:45:18 +0300

    schimbari css

--- src/components/CurrentLocation.jsx
+++ src/components/CurrentLocation.jsx
@@ -2,6 +2,6 @@ export function CurrentLocation(props){
     // pe props o sa primim orasul
     const { currentLocation } = props;
     return(
-        <p>Orasul tau current este <strong>{currentLocation}</strong></p>
+        <p className="custom-writing">Orasul tau current este <strong>{currentLocation}</strong></p>
     )
 }
